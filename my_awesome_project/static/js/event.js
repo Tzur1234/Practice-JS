fb4eebe8445f405e4f5a0c0645e6fcebcd6fa050
root <root@DESKTOP-O1KC17T>
2023-04-13 22:44:55 +0300

    add -r requirements/production.txt

--- my_awesome_project/static/js/event.js
+++ my_awesome_project/static/js/event.js
@@ -1,23 +1,23 @@
 
 
-function showEvent(event){
+function showEvent(event) {
     // Fetch insert div element
     items = document.querySelectorAll('.key')
 
     // set key
     pressed_key = document.createTextNode(event.key)
-    items[0].replaceChild(pressed_key,items[0].firstChild)
-    
+    items[0].replaceChild(pressed_key, items[0].firstChild)
+
     // set KeyCode
     items[1].replaceChild(document.createTextNode(`${event.keyCode}`), items[1].firstChild)
-    
+
     // set code
     console.log(event.code)
     items[2].replaceChild(document.createTextNode(`${event.code}`), items[2].firstChild)
 
 
 }
-function showEvent2(event){
+function showEvent2(event) {
     // Fetch insert div element
     item1 = document.querySelector('.key:nth-child(1)')
     item2 = document.querySelector('.key:nth-child(2)')
@@ -29,11 +29,11 @@ function showEvent2(event){
 
     // set key
     pressed_key = document.createTextNode(event.key)
-    item1.replaceChild(pressed_key,item1.firstChild)
-    
+    item1.replaceChild(pressed_key, item1.firstChild)
+
     // set KeyCode
     item2.replaceChild(document.createTextNode(`${event.keyCode}`), item2.firstChild)
-    
+
     // set code
     console.log(event.code)
     item3.replaceChild(document.createTextNode(`${event.code}`), item3.firstChild)
@@ -41,6 +41,43 @@ function showEvent2(event){
 
 }
 
+function showEvent3(event) {
+
+    // Delete all events
+
+    document.querySelector('#insert').innerHTML = ''
+
+    const KeyCodes = {
+        'event.key': event.key,
+        'event.KeyCode': event.KeyCode,
+        'event.code': event.code,
+    }
+    console.log(event.keyCode)
+
+    for ( key in KeyCodes){
+        // div
+        div = document.createElement('div')
+        div.className = 'key'
+
+        // value
+        value = document.createTextNode(`${KeyCodes[key]}`)
+        
+
+        // small 
+        small = document.createElement('small')
+        small.appendChild(document.createTextNode(`${key}`))
+
+        // append all together
+
+        div.appendChild(small)
+        div.appendChild(value)
+        document.querySelector('#insert').appendChild(div)
+
+    }
+
+
+}
+
 
 // window.addEventListener('keyup', showEvent)
-window.addEventListener('keyup', showEvent2)
+window.addEventListener('keyup', showEvent3)
